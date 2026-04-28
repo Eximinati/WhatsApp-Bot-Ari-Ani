01ee412754983a301af243671a4a72f6b39cf34b
Deryl <153626742+Push-b@users.noreply.github.com>
2026-04-28 20:24:47 +0200

    Update media.js

--- src/utils/media.js
+++ src/utils/media.js
@@ -3,6 +3,7 @@ const os = require("os");
 const path = require("path");
 const crypto = require("crypto");
 const ffmpeg = require("fluent-ffmpeg");
+const axios = require("axios");
 const { downloadContentFromMessage } = require("@whiskeysockets/baileys");
 const { Sticker, StickerTypes } = require("wa-sticker-formatter");
 
@@ -27,12 +28,18 @@ async function downloadMessageBuffer(message) {
   const messageType = message.mtype
     ? message.mtype.replace(/Message/gi, "")
     : mime.split("/")[0];
-  const stream = await downloadContentFromMessage(source, messageType);
+
+  const stream = await downloadContentFromMessage(
+    source,
+    messageType
+  );
+
   return streamToBuffer(stream);
 }
 
 async function createStickerFromMessage(message, options) {
   const buffer = await downloadMessageBuffer(message);
+
   const sticker = new Sticker(buffer, {
     type: StickerTypes.FULL,
     quality: 80,
@@ -61,22 +68,115 @@ function convertWebpToPngBuffer(buffer) {
           } catch (error) {
             reject(error);
           } finally {
-            await Promise.allSettled([fs.unlink(inputPath), fs.unlink(outputPath)]);
+            await Promise.allSettled([
+              fs.unlink(inputPath),
+              fs.unlink(outputPath)
+            ]);
           }
         })
         .on("error", async (error) => {
-          await Promise.allSettled([fs.unlink(inputPath), fs.unlink(outputPath)]);
+          await Promise.allSettled([
+            fs.unlink(inputPath),
+            fs.unlink(outputPath)
+          ]);
           reject(error);
         });
+
     } catch (error) {
-      await Promise.allSettled([fs.unlink(inputPath), fs.unlink(outputPath)]);
+      await Promise.allSettled([
+        fs.unlink(inputPath),
+        fs.unlink(outputPath)
+      ]);
       reject(error);
     }
   });
+}
+
+function mediaInput(data) {
+  return Buffer.isBuffer(data)
+    ? data
+    : typeof data === "string"
+    ? { url: data }
+    : data;
+}
+
+async function getAudioBuffer(url) {
+  const res = await axios.get(url,{
+    responseType:"arraybuffer"
+  });
+
+  return Buffer.from(res.data);
+}
+
+async function sendVideoMessage(
+  conn,
+  from,
+  url,
+  M
+){
+  return conn.sendMessage(
+    from,
+    {
+      video: mediaInput(url),
+      caption: "> Developed By *Deryl*",
+    },
+    { quoted:M }
+  );
+}
+
+async function sendAudioMessage(
+  conn,
+  from,
+  url,
+  M,
+  ppt=false,
+  contextInfo={}
+){
+
+  if (
+    typeof url==="string" &&
+    /^https?:\/\//.test(url)
+  ){
+    url = await getAudioBuffer(url);
+  }
+
+  return conn.sendMessage(
+    from,
+    {
+      audio: mediaInput(url),
+      mimetype:"audio/mpeg",
+      ptt:ppt,
+      contextInfo:{
+        ...contextInfo
+      }
+    },
+    { quoted:M }
+  );
+}
+
+async function sendDocumentMessage(
+  conn,
+  from,
+  url,
+  M
+){
+  return conn.sendMessage(
+    from,
+    {
+      document: mediaInput(url),
+      caption: "> Developed By *Deryl*",
+      mimetype:"application/pdf",
+      fileName:`${Date.now()}.pdf`
+    },
+    { quoted:M }
+  );
 }
 
 module.exports = {
   convertWebpToPngBuffer,
   createStickerFromMessage,
   downloadMessageBuffer,
+  sendVideoMessage,
+  sendAudioMessage,
+  sendDocumentMessage
 };
