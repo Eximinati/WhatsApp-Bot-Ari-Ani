2efb3a2b105a7c712be87df5420321046c3b977f
Deryl <153626742+Push-b@users.noreply.github.com>
2026-05-13 08:55:38 +0200

    Refactor ICommand interface for better readability

--- src/typings/command.d.ts
+++ src/typings/command.d.ts
@@ -1,20 +1,35 @@
 import MessagePipeline from '../pipeline/MessagePipeline.js'
 import RuntimeClient from '../core/RuntimeClient.js'
-import type { IParsedArgs, ISimplifiedMessage } from './index.js'
+import type {
+    IParsedArgs,
+    ISimplifiedMessage
+} from './index.js'
 
 export interface ICommand {
     client?: RuntimeClient
     handler?: MessagePipeline
-    run(M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void | never> | void | never
+
+    run(
+        M: ISimplifiedMessage,
+        parsedArgs: IParsedArgs
+    ): Promise<void | never> | void | never
+
     config: {
         adminOnly?: boolean
         aliases?: string[]
         description?: string
+
         command: string
+
         id?: string
+        name?: string
+
         category?: TCategory
         usage?: string
+
         dm?: boolean
+        groupOnly?: boolean
+
         baseXp?: number
         modsOnly?: boolean
     }
